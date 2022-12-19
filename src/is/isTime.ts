afa907e015f87f7a96924b426afe443a0c4cd3a4
grh-gitee <3494242908@qq.com>
2022-12-19 15:57:46 +0800

    feat: 移除 isSameDate 方法, 升级版本

--- src/is/isTime.ts
+++ src/is/isTime.ts
@@ -49,20 +49,3 @@ export function isTime(time: any): boolean {
 	}
 	return time instanceof Date && !isNaN(time.getTime());
 }
-
-/**
- * @title isSameDate
- * @description 时间是否相同, 时间类型支持isDate的类型
- * @param timeA 比较时间
- * @param timeB 被比较时间
- * @returns {boolean}
- * @version 0.1.0
- */
-export function isSameDate(timeA: Time, timeB: Time): boolean {
-
-	if (!isTime(timeA) || !isTime(timeB)) return false
-	if (timeA.toString() === timeB.toString()) return true
-	if (new Date(timeA).getTime() === new Date(timeB).getTime()) return true
-
-	return false
-}
