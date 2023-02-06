c433f1d1da8e15718f946442fe0092299bf663a2
grh-gitee <3494242908@qq.com>
2023-02-06 22:43:35 +0800

    feat: 优化多个方法的备注和代码

--- src/is/isArray.ts
+++ src/is/isArray.ts
@@ -1,7 +1,7 @@
 /**
  * @title isArray
  * @description 是否为数组
- * @param value any
- * @returns boolean
+ * @param value {unknown}
+ * @returns {boolean}
  */
 export const isArray = Array.isArray
--- src/is/isNumber.ts
+++ src/is/isNumber.ts
@@ -1,14 +1,16 @@
 import { type } from './type'
 import { INFINITY } from '../constants'
 import { isString } from './isString'
+import { isEmpty } from './isUtil'
+import { isArray } from './isArray'
 
 /**
  * @title isEffectNumber
  * @description 是否为js的有效区间的数, 非number类型都为false
- * @param num any
+ * @param num
  * @returns boolean
  */
-export function isEffectNumber(num: any): boolean {
+export function isEffectNumber(num: unknown): boolean {
 	if (type(num) === 'Number') {
 		if (num === INFINITY || num === -INFINITY) return false
 		return true
@@ -22,7 +24,8 @@ export function isEffectNumber(num: any): boolean {
  * @param num 待检测的数据类型
  * @returns boolean
  */
-export function isFloat(num: number): boolean {
+export function isFloat(num: unknown): boolean {
+	if(!isNumber(num)) return false
 	return (num % 1) !== 0
 }
 
@@ -40,12 +43,12 @@ export const isNumber = (val: unknown): val is number => type(val) === 'Number'
  * @param value 待检测的数据类型
  * @returns {boolean}
  */
-export const isBigInt = (value:unknown): value is bigint => type(value) === 'BigInt'
+export const isBigInt = (value: unknown): value is bigint => type(value) === 'BigInt'
 
 
 /**
  * @title likeNumber
- * @description 是否为数字
+ * @description 是否为数字/数字字符串
  * @support: Number, NumberString
  * @unsupported: Infinity, Function
  * @param value any
@@ -54,17 +57,16 @@ export const isBigInt = (value:unknown): value is bigint => type(value) === 'Big
  */
 export function likeNumber(value: unknown): boolean {
 
-	if (isString(value)) {
-		value = value.replaceAll(' ', '')
-	}
+	if (isNumber(value)) return true
 
-	if ([Infinity, null, undefined, '', NaN].includes(value as any)) return false
+	if (isString(value)) value = value.trim()
 
-	if (Array.isArray(value)) return false
-
-	const result = type(value) === "Number" && typeof value === 'number'
-
-	if (result) return result
+	if (
+		isEmpty(value)
+		|| isArray(value)
+		|| value === ''
+		|| isNaN(Number(value))
+	) return false
 
 	if (Number.isNaN(Number(value)) === false) {
 		return true
